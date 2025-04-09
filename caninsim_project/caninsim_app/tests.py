989211138d2052ccdf43b80ae6b4fbd6ca86a6e6
jkrepository <justyna.klewicka@outlook.com>
2025-04-09 13:53:31 +0200

    SCRUM-17 Write tests for views and coverage setup

--- caninsim_project/caninsim_app/tests.py
+++ caninsim_project/caninsim_app/tests.py
@@ -19,3 +19,29 @@ class DogModelTest(TestCase):
     def test_get_absolute_url(self):
         expected_url = reverse("dog_detail", kwargs={"pk": self.dog.pk})
         self.assertEqual(self.dog.get_absolute_url(), expected_url)
+
+    def test_index_view_counts(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['num_dogs'], 1)
+        self.assertEqual(response.context['num_users'], 1)
+
+    def test_dog_list_view(self):
+        response = self.client.get(reverse('dogs'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.dog.dog_name)
+
+    def test_dog_detail_view(self):
+        response = self.client.get(reverse('dog_detail', kwargs={'pk': self.dog.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.dog.dog_name)
+
+    def test_user_list_view(self):
+        response = self.client.get(reverse('users'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.user.username)
+
+    def test_user_detail_view(self):
+        response = self.client.get(reverse('user_detail', kwargs={'pk': self.user.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.user.username)
