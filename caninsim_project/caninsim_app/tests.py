b97a85642fda135d09c7fc1e2d4d31a91559a564
jkrepository <justyna.klewicka@outlook.com>
2025-04-09 13:39:44 +0200

    SCRUM-17 Create tests for Dog model

--- caninsim_project/caninsim_app/tests.py
+++ caninsim_project/caninsim_app/tests.py
@@ -1,6 +1,21 @@
 from django.test import TestCase
+from .models import Dog
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 class SimpleTest(TestCase):
     def test_basic(self):
         self.assertEqual(1, 1)
 
+class DogModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="tester", password="pass")
+
+    def test_str_returns_dog_name(self):
+        dog = Dog.objects.create(dog_name="Shadowfang", owner=self.user)
+        self.assertEqual(str(dog), "Shadowfang")
+
+    def test_get_absolute_url(self):
+        dog = Dog.objects.create(dog_name="Willow", owner=self.user)
+        expected_url = reverse("dog_detail", kwargs={"pk": dog.pk})
+        self.assertEqual(dog.get_absolute_url(), expected_url)
